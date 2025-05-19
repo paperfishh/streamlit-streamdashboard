fc60162a16b2c741a98170077de0446fc0fce97c
Ziaul Haq <zhaqi035@gmail.com>
2025-05-19 16:53:55 +0700

    Remove sensitive config files from repo and update .gitignore

--- config/pg_config.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy import create_engine
-
-def get_pg_engine():
-    return create_engine("postgresql://postgres:admin@localhost:5433/iot_db")
--- config/redis_config.py
+++
@@ -1,4 +0,0 @@
-import redis
-
-def get_redis_connection():
-    return redis.Redis(host='172.17.125.76', port=6379, decode_responses=True)
